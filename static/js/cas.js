0baf5e221ae3e7f429c5e48d50c17ace1b63d16f
Zorawar Moolenaar <zorawar87@gmail.com>
2019-04-07 01:34:49 +0000

    views

---
+++ static/js/cas.js
@@ -0,0 +1,25 @@
+
+$("#btn1").click(function(event) {
+    event.preventDefault();
+    var input = $('#input').val();
+    
+  $("#output").html(input + " lorem ipsum");
+});
+
+$("#btn2").click(function(event) {
+    event.preventDefault();
+    var input = $('#input').val();
+    
+  $("#output").html(input + " lorem ipsum");
+});
+
+$("#btn3").click(function(event) {
+    event.preventDefault();
+    var input = $('#input').val();
+    
+  $("#output").html(input + " lorem ipsum");
+});
+
+$("#submit").click(function(event) {
+   $("#contents").toggle();
+});
